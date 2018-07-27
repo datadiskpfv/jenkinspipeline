9853cde37fa90296b1827d3d8e776354b021e2cc
pvalle <paul.valle@datadisk.co.uk>
2018-07-27 08:56:45 +0100

    added stage one of pipeline, change for windows

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -17,5 +17,9 @@ node {
         def MVN_HOME = "${tool 'Maven-3.5.4'}/bin"
 
         echo "MAVEN_HOME: $MVN_HOME"
+
+        def pomVersion = bat(script: 'mvn -q -Dexec.executable=\'echo\' -Dexec.args=\'${project.version}\' --non-recursive exec:exec', returnStdout: true).trim()
+        echo "POM Version: ${pomVersion}"
+
     }
 }
