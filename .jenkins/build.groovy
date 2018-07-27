10de27582e57dfd90ac2f83cdee5963d961533e6
pvalle <paul.valle@datadisk.co.uk>
2018-07-27 08:43:39 +0100

    added stage one of pipeline

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -1,3 +1,13 @@
 node {
     echo 'The pipeline started'
+
+    def branchVersion = ""
+
+    stage ('Checkout') {
+        // checkout repository
+        checkout scm
+
+        // checkout input branch
+        sh "git checkout ${caller.env.BRANCH_NAME}"
+    }
 }
